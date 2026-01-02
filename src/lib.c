37ca4ae6d20273d4b88978a29774f49e2b069fd8
sueszli <jabaryyahya@gmail.com>
2026-01-02 22:15:15 +0100

    up

---
+++ src/lib.c
@@ -0,0 +1,4 @@
+#include "lib.h"
+#include <stdio.h>
+
+void hello_from_lib(void) { printf("Hello from lib!\n"); }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+#include "lib.h"
 #include <stdio.h>
 
 int main(void) {
     printf("Hello, World!\n");
+    hello_from_lib();
     return 0;
 }
