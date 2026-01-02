8310a3360ee2e650f33562e6f8851527346d8a85
sueszli <jabaryyahya@gmail.com>
2026-01-02 23:46:31 +0100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "lib.h"
 #include <print>
 
+#include "raylib.h"
+
 int main() {
     std::println("Hello, World!");
     hello_from_lib();
+
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
