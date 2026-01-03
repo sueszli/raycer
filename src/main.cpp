38db60c3810ae8e80f82865f878c55ece37e6ac1
sueszli <jabaryyahya@gmail.com>
2026-01-03 03:47:28 +0100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
-#include "lib.h"
-#include <print>
-
 #include "raylib.h"
+#include <cstdlib>
 
 int main() {
-    std::println("Hello, World!");
-    hello_from_lib();
+    InitWindow(800, 450, "raycer");
 
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    Vector3 carPosition = {0.0f, 0.0f, 0.0f};
+
+    Camera3D camera = {};
+    camera.position = (Vector3){0.0f, 10.0f, 10.0f};
+    camera.target = carPosition;
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        DrawText("Press ESC to exit gracefully", 190, 230, 20, DARKGRAY);
+
+        DrawFPS(10, 10);
+
+        BeginMode3D(camera);
+
+        DrawGrid(10, 1.0f);
+
+        DrawCube(carPosition, 2.0f, 1.0f, 4.0f, RED);
+
+        DrawCubeWires(carPosition, 2.0f, 1.0f, 4.0f, MAROON);
+
+        EndMode3D();
+
         EndDrawing();
     }
 
     CloseWindow();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
